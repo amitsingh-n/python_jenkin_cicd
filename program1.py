fbf6447107947f1e47db178802219e800dba0563
Amit <amitcd100@gmail.com>
2021-01-20 18:10:57 +0530

    added function add_two number

--- program1.py
+++ program1.py
@@ -1,2 +1,15 @@
 
 print("This is test program to check the Jenkins")
+
+def add_number(one,two):
+    """
+     function is adding two number one and two
+    """
+    return one + two
+
+
+result = add_number(4,5)
+print("sum of two number is : {}".format(result))
+
+
+
